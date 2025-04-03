5fe87346e376cb2bfb67a8f26b981063cfc200c7
rsayyed591 <rehansayyed591@gmail.com>
2025-04-03 16:10:21 +0530

    Updated Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <div className="bg-[#f2f2f2] dark:bg-[#1c1c24] rounded-[20px] w-full  flex items-center justify-center text-black dark:text-white gap-2 font-epilogue p-4 mt-10 text-center shadow-md ">
+    <div className="bg-white dark:bg-slate-800 rounded-2xl w-full flex items-center justify-center theme-text-primary gap-2 font-epilogue p-4 mt-10 text-center shadow-soft">
       Copyright &copy; {1900 + new Date().getYear()}
-      <a target="_blank" href="https://github.com/thekiranmahajan/FundVerse">
-        <span className="inline-block text-center px-4 py-2 rounded-lg bg-[#c8cbcecb] dark:bg-[#2c2f32] text-[#6F01Ec]  font-semibold">
-          FundVerse
+      <a target="_blank" href="https://github.com/AyeshaFatima22/kindcoin" rel="noreferrer">
+        <span className="inline-block text-center px-4 py-2 rounded-xl bg-pink-50 dark:bg-purple-900 text-pink-600 dark:text-pink-300 font-semibold transition-all hover:bg-pink-100 dark:hover:bg-purple-800">
+          KindCoin
         </span>
       </a>
       All Rights Reserved.
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
+
