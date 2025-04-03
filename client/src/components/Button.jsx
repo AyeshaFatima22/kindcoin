e181c1a8f4e509148f656f059ed29372f32279a9
rsayyed591 <rehansayyed591@gmail.com>
2025-04-03 16:07:07 +0530

    Updated Button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ type, title, handleClick, styles, className = "", children, is
   return (
     <button
       type={type}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles} ${className}`}
+      className={`font-quicksand font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-xl shadow-button transition-all duration-300 hover:scale-[0.98] focus:scale-[1.02] ${styles} ${className}`}
       onClick={handleClick}
       disabled={isDisabled}
     >
