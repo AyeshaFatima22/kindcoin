63d58e38afe98b2a06e72c0b3363b345c5f89fa6
rsayyed591 <rehansayyed591@gmail.com>
2025-04-03 16:11:52 +0530

    Updated Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { logo } from "../assets";
-import { navlinks } from "../constants";
-import Icon from "./Icon";
-import ThemeModes from "./ThemeModes";
+import { NavLink } from "react-router-dom"
+import { logo } from "../assets"
+import { navlinks } from "../constants"
+import Icon from "./Icon"
+import ThemeModes from "./ThemeModes"
 
 const Sidebar = () => {
   return (
     <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh] select-none">
       <NavLink to="/">
-        <Icon
-          name="FundVerse"
-          styles="w-[52px] h-[52px] bg-[#f0f0f0] dark:bg-[#2c2f32] shadow-md "
-          imgUrl={logo}
-        />
+        <Icon name="KindCoin" styles="w-[52px] h-[52px] bg-white dark:bg-slate-800 shadow-soft" imgUrl={logo} />
       </NavLink>
 
-      <div className="flex-1 flex flex-col justify-between items-center bg-[#f2f2f2] dark:bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12 shadow-md">
+      <div className="flex-1 flex flex-col justify-between items-center bg-white dark:bg-slate-800 rounded-2xl w-[76px] py-4 mt-12 shadow-soft">
         <div className="flex flex-col justify-center items-center gap-3">
           {navlinks.map((Link) => (
             <NavLink key={Link.name} to={Link.route}>
@@ -30,7 +25,8 @@ const Sidebar = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Sidebar;
+export default Sidebar
+
