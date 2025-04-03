17634884918d8ef61c9a796bf686dd5f62ff02c0
rsayyed591 <rehansayyed591@gmail.com>
2025-04-03 16:08:18 +0530

    Updated CustomButton

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,24 +1,17 @@
-import React from "react";
-
-const CustomButton = ({
-  btnType,
-  title,
-  handleClick,
-  styles,
-  isDisabled = false,
-}) => {
+const CustomButton = ({ btnType, title, handleClick, styles, isDisabled = false }) => {
   return (
     <button
       type={btnType}
-      className={`font-epilogue font-semibold  dark:text-black text-white min-h-[52px] px-4 rounded-xl transition-all duration-300 hover:scale-95 focus:scale-105 outline-none focus:ring-4 ring-[#6F01Ec] shadow-md ${styles} ${
-        isDisabled && "grayscale cursor-not-allowed"
+      className={`font-quicksand font-semibold dark:text-white text-white min-h-[52px] px-4 rounded-xl transition-all duration-300 hover:scale-98 focus:scale-102 outline-none focus:ring-4 ring-pink-200 dark:ring-purple-400 shadow-button ${styles} ${
+        isDisabled && "opacity-60 cursor-not-allowed"
       }`}
       disabled={isDisabled}
       onClick={handleClick}
     >
       {title}
     </button>
-  );
-};
+  )
+}
 
-export default CustomButton;
+export default CustomButton
+
